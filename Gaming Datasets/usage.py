e918f2982f9bcbe0399905b586fa2210c21c2a4b
Meysam Agah <113939902+MeysamAgah@users.noreply.github.com>
2025-01-08 23:30:30 +0330

    Update usage.py

--- Gaming Datasets/usage.py
+++ Gaming Datasets/usage.py
@@ -1,1 +1,7 @@
+# very very very simple
+## for example
+import pandas as pd
 
+#for url just click on desired dataset csv then click on Raw and then copy url
+url = 'https://raw.githubusercontent.com/MeysamAgah/Datasets/refs/heads/main/Gaming%20Datasets/Elden_Ring_MetaCritic_User_PS5.csv'
+df = pd.read_csv(url)
